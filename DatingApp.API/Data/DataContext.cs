c5c46ffadc8e78577c19dc1cd9b39070b71239d1
Nestor <miaritisnestor@gmail.com>
2020-09-30 13:59:47 +0300

    Initial Create

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using DatingApp.API.Models;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
